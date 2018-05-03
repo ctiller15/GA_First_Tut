f5769e86ff550fccc5cc972342b5134015325c3c
Tiller <ctiller@proforma.com>
2018-05-03 13:12:50 -0400

    Add project files.

---
+++ GA_Article_Medium/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Article_Medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
